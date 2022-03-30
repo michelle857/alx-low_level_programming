fdf8cd51fc0a18408dfb72e9fb4afc6d71de1ad0
michelle857 <michellemashamba2001@gmail.com>
2022-03-30 00:32:39 -0700

    main

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char c);
+int _evaluate(int i, int n);
+int evaluate_num(int num, int iterator);
